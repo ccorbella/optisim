035b53ca6513f8d7923eff09070178420b9779f6
conrad.corbella <conradcorbella@hotmail.com>
2016-10-15 19:40:46 +0200

    Algunes modificacions. S'afegeix el return de la mitjana del temps d'espera dels clients i del nombre de persones que estan esperant.

--- programa.py
+++ programa.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import random
 
 def numero_articles():
-    """ Retorna el número d'articles que ha demanat el client, seguint
-    la funció de probabilitat proporcionada a l'enunciat."""
+    """ Retorna el numero d'articles que ha demanat el client, seguint
+    la funcio de probabilitat proporcionada a l'enunciat. """
     return np.random.choice([3,8,12,18,30],p=[0.1,0.2,0.25,0.15,0.3])
 
 def temps_cobrar(articles):
@@ -15,10 +17,15 @@ def supermercat(T,nl,N):
     propera_sortida   = 1000
     properes_sortides = np.zeros(N)
     t                 = 0
+
+    espera_clients    = np.empty(T)
+    temps_espera      = np.empty(0)
     
     data = {k: [] for k in xrange(N)}
     
     while (t < T) or (caixes.sum() > 0):
+
+        to = t
         
         #Mirem quin és el proper esdeveniment. Si el primer que passa és que arriba un nou client,
         if  ((t == propera_entrada) or (caixes.sum() == 0)) and (t < T):
@@ -45,8 +52,12 @@ def supermercat(T,nl,N):
             min_sortides                     = np.min(properes_sortides[caixes != 0])
             idx_caixa                        = np.where(properes_sortides == min_sortides)[0][0]
             element = data[idx_caixa].pop(0) #eliminem el primer element de la llista
-            properes_sortides[idx_caixa]     = element[0] + temps_cobrar(element[1])
             caixes[idx_caixa]               -= 1
+
+            if caixes[idx_caixa] > 0:
+                properes_sortides[idx_caixa]     = data[idx_caixa][0][0] + temps_cobrar(data[idx_caixa][0][1])
+            
+            temps_espera                     = np.append(temps_espera,t-element[0])
             
             if (caixes > caixes[idx_caixa]).sum() > 0:
                 idx = np.where(caixes > caixes[idx_caixa])[0][0]
@@ -55,5 +66,8 @@ def supermercat(T,nl,N):
                 caixes[idx]       -= 1
                 
         t = min(propera_entrada,propera_sortida)
+        espera_clients[to:t] = caixes.sum()
+
+    return np.mean(espera_clients), np.mean(temps_espera)
         
 print(supermercat(2000,17,20))
